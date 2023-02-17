30ea0243a2433329e28aa8ef6572470983b022f0
Zein19 <zein.23@hotmail.co.uk>
2023-02-17 15:07:42 +0000

    added props to the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card(){
+function Card(props){
     return(
         <div className="card">
-            <img src="/images/katie-zaferes.png" className="card-image" alt="katie zaferes" />
+            <img src={props.img} className="card-image" alt="katie zaferes" />
             <div className="card-info">
                 <img src="/images/star.png" className="card-star" alt="star"/>
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
